0abf2edacf382a0bdddb58d3eb8c7cfe5ce909dc
Jonathan <you@example.com>
2015-06-22 21:02:17 +0100

    added server code

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,25 +1,32 @@
 package server;
 
-import static java.rmi.Naming.rebind;
 import static java.rmi.registry.LocateRegistry.createRegistry;
+import static java.rmi.server.UnicastRemoteObject.exportObject;
 
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 
-import transport.remote.Factory;
-import transport.remote.FactoryInterface;
+import transport.remote.RemoteService;
+import transport.remote.RemoteServiceInterface;
 
 public class Server {
 
-	private static int port = 1099;
+	private static int port = 1236;
 
-	public static void register() throws RemoteException, MalformedURLException {
-		createRegistry(port);
-		FactoryInterface factory = new Factory();
-		rebind("factory", factory);
+	public static void register() throws RemoteException, MalformedURLException, InterruptedException {
+
+		RemoteServiceInterface service = new RemoteService();
+		RemoteServiceInterface serviceStub = (RemoteServiceInterface) exportObject(service, port);
+		Registry registry = createRegistry(port);
+		// Registry r=LocateRegistry.g
+		registry.rebind("service", serviceStub);
+		Thread.sleep(8000);
+		// UnicastRemoteObject.unexportObject(service, Boolean.FALSE);
 	}
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException {
+	public static void main(String[] args) throws RemoteException, MalformedURLException, InterruptedException {
 		new Server().register();
 	}
+	// registry.rebind("//localhost:" + port + "/factory", serviceStub)
 }
